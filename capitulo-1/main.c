1b1e86fa9a906f3c563ca196e09b0e70d93b19cf
RRICHARRD <guilherme.rixard@gmail.com>
2021-04-29 21:49:35 -0300

    test first printf command

---
+++ capitulo-1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+	
+	printf("Learning\n");
+	printf("programming in C language\n");
+	printf("to study data structures in the future\n"); 
+	
+	system("pause");
+}
